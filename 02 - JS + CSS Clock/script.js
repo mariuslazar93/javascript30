237fdda8bb9ca03711898f9bdcc734ddb2d0f6e5
mariuslazar93 <mariuslazar93@yahoo.ro>
2016-12-12 02:17:13 +0200

    init second challenge

---
+++ 02 - JS + CSS Clock/script.js
@@ -0,0 +1,15 @@
+var clockComponent = function(){
+
+    function bindEvents(){
+
+    }
+    function init(){
+        
+    }
+
+    return {
+        init: init
+    }
+}();
+
+clockComponent.init();
