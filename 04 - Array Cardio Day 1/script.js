61951851feae85281b45f24aa8548a268ccabe2d
mariuslazar93 <mariuslazar93@yahoo.ro>
2016-12-18 23:34:52 +0200

    finish day 4

--- 04 - Array Cardio Day 1/script.js
+++ 04 - Array Cardio Day 1/script.js
@@ -20,7 +20,7 @@ const inventors = [
 
 const flavours = ['Chocolate Chip', 'Kulfi', 'Caramel Praline', 'Chocolate', 'Burnt Caramel', 'Pistachio', 'Rose', 'Sweet Coconut', 'Lemon Cookie', 'Toffeeness', 'Toasted Almond', 'Black Raspberry Crunch', 'Chocolate Brownies', 'Pistachio Almond', 'Strawberry', 'Lavender Honey', 'Lychee', 'Peach', 'Black Walnut', 'Birthday Cake', 'Mexican Chocolate', 'Mocha Almond Fudge', 'Raspberry'];
 
-const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
@@ -56,11 +56,11 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 
     array.sort(function(a,b){
         if(a.year < b.year){
-            return 1;
+            return -1;
         }
         else {
             if(a.year > b.year){
-                return -1;
+                return 1;
             }
             else{
                 return 0;
@@ -79,18 +79,94 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 // 4. How many years did all the inventors live?
 
 (function(array){
-    console.log
+    console.log("4. How many years did all the inventors live?");
+    var totalYears = array.reduce(function(acc, elem){
+        return acc+(elem.passed - elem.year);
+    },0)
+    console.log("Total years: " + totalYears);
 })(inventors);
 
 // 5. Sort the inventors by years lived
 
+(function(array){
+    console.log("5. Sort the inventors by years lived");
+
+    array.sort(function(a,b){
+        var yearsLivedByA = a.passed - a.year;
+        var yearsLivedByB = b.passed - b.year;
+        if(yearsLivedByA < yearsLivedByB){
+            return -1;
+        }
+        else {
+            if(yearsLivedByA > yearsLivedByB){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+    });
+
+    var names = array.map(function(elem){
+        return elem.first + " " + elem.last;
+    });
+    console.log(names);
+})(inventors);
+
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
+(function(){
+    // var category = document.querySelector('.mw-category');
+    // var links = [].slice.call(category.querySelectorAll('a'));
+    // var de = links
+    //             .map(link => link.textContent)
+    //             .filter(streetName => streetName.includes('de'));
+})();
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
 
+(function(array){
+    console.log("7. Sort the people alphabetically by last name");
+
+    function getLastName(fullName){
+        return fullName.split(',')[0];
+    };
+
+    array.sort(function(a,b){
+        var lastNameOfA = getLastName(a).toLowerCase();
+        var lastNameOfB = getLastName(b).toLowerCase();
+        if(lastNameOfA < lastNameOfB){
+            return -1;
+        }
+        else {
+            if(lastNameOfA > lastNameOfB){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+    });
+
+    console.log(array);
+})(people);
+
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+(function(data){
+    console.log('8. Sum up the instances of each of these: ' + data);
+    var results = data.reduce(function(acc, elem){
+        if(acc[elem])
+            acc[elem]++;
+        else {
+            acc[elem] = 1;
+        }
+        return acc;
+    },{});
+
+    console.log(results);
+})(data);
